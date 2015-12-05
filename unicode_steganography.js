de82b6c29d9a3a78c77416e7d64deef17a99b7a3
330k <kei.misawa@gmail.com>
2015-12-05 18:30:43 +0900

    Update unicode_steganography.js

--- unicode_steganography.js
+++ unicode_steganography.js
@@ -33,8 +33,8 @@ function encode_steganography(text1, text2){
     });
     var embed_string = (function(str1, str2){
         var result = [];
-        var c1 = str1.split('');
-        var c2 = str2.split('.{8}');
+        var c1 = str1.split(/./g);
+        var c2 = str2.split(/.{8}/g);
         var ratio = c1.length / (c1.length + c2.length);
         
         while((c1.length > 0) && (c2.length > 0)){
