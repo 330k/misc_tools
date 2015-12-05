694f119432f0a4facc599dacdd7b0ee585576e9a
330k <kei.misawa@gmail.com>
2015-12-05 18:26:36 +0900

    Create unicode_steganography.js

---
+++ unicode_steganography.js
@@ -0,0 +1,81 @@
+/**
+ Zero-Width Unicode Character Steganography Encoder
+  args: original text, hidden text
+  return: unicode text with steganography
+ */
+function encode_steganography(text1, text2){
+    var encode_to_zerowidth_characters = (function(str1){
+        var chars = ['\u200b', '\u200c', '\u200d', '\u202c'];
+        var radix = chars.length;
+        var result = [];
+        var base = '0'.repeat(Math.ceil(Math.log2(65536) / Math.log2(radix)));
+        
+        for(var i = 0; i < str1.length; i++){
+            var c = str1.charCodeAt(i);
+            var d = c.toString(radix);
+            
+            result[i] = (base + d).substr(-8);
+        }
+        
+        var r = result.join('');
+        
+        for(var i = 0; i < radix; i++){
+            r = r.replace(new RegExp(i, 'g'), chars[i]);
+        }
+        
+        return r;
+    });
+    var embed_string = (function(str1, str2){
+        var result = [];
+        var c1 = str1.split('');
+        var c2 = str2.split('');
+        var ratio = c1.length / (c1.length + c2.length);
+        
+        while((c1.length > 0) && (c2.length > 0)){
+            if(Math.random() <= ratio){
+                result.push(c1.shift());
+            }else{
+                result.push(c2.shift());
+            }
+        }
+        result = result.concat(c1).concat(c2);
+        
+        return result.join('');
+    });
+    return embed_string(text1, encode_to_zerowidth_characters(text2));
+}
+
+/**
+ Zero-Width Unicode Character Steganography Decoder
+  args: unicode text with steganography
+  return: array of [original text, hidden text]
+ */
+function decode_steganography(text1){
+    var chars = ['\u200b', '\u200c', '\u200d', '\u202c'];
+    var split_zerowidth_characters = (function(str1){
+    	var result = [];
+    	result[0] = str1.replace(new RegExp('[' + chars.join('') + ']', 'g'), '');
+    	result[1] = str1.replace(new RegExp('[^' + chars.join('') + ']', 'g'), '');
+    	
+    	return result;
+    });
+    var decode_from_zero_width_characters = (function(str1){
+        var radix = chars.length;
+        var codelength = Math.ceil(Math.log2(65536) / Math.log2(radix));
+        
+        var r = str1;
+        for(var i = 0; i < radix; i++){
+            r = r.replace(new RegExp(chars[i], 'g'), i);
+        }
+        var result = [];
+        for(var i = 0; i < r.length; i += codelength){
+            result.push(String.fromCodePoint(Number.parseInt(r.substr(i, codelength), radix)));
+        }
+        
+        return result.join('');
+    });
+    
+    var result = split_zerowidth_characters(text1);
+    result[1] = decode_from_zero_width_characters(result[1]);
+    return result;
+}
